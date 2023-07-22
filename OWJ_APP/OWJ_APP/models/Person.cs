5e5287daa6fd4f9e361b826781ce1586599362fd
A.Ardam <A.Ardam@etadbir.com>
2023-07-22 16:51:41 +0330

    Add models

---
+++ OWJ_APP/OWJ_APP/models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OWJ_APP.enums;
+namespace OWJ_APP.models
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string FullName { get; set; }
+        public double IDNumber { get; set; }
+        public Languages languages { get; set; }
+        public string Email { get; set; }
+        public string TelOffice { get; set; }
+        public string AdditinalInformation { get; set; }
+
+    }
+}
